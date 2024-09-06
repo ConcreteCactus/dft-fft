d61c7c9092f76c3d31ebb107d21e5b28e501286d
Áron Hárnási <aron.harnasi@gmail.com>
2024-09-06 17:48:31 +0200

    separated dft lib to different translation unit

--- include/dft.h
+++ include/dft.h
@@ -1,13 +1,17 @@
+#ifndef DFT_H_
+#define DFT_H_
+
 #include <stdint.h>
 
 typedef struct {
     float r, i; // r: real (cos), i: imaginary (sin)
 } dft_cmplx_t;
 
-void dft_hello(void);
 void dft_transform(const float* samples, const uint32_t samples_size, 
         const float samples_length, const float min_freq,
         const uint32_t transform_max_size,
         float* transformation);
 void dft_fft(dft_cmplx_t* result, const float* samples,
         const uint32_t samples_size);
+
+#endif
---
+++ include/dft_hello.h
@@ -0,0 +1,6 @@
+#ifndef DFT_HELLO_H_
+#define DFT_HELLO_H_
+
+void dft_hello(void);
+
+#endif
